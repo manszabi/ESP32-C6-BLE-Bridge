f36f05d829cef5359963548c74b498e2ce87df34
manszabi <42097377+manszabi@users.noreply.github.com>
2026-03-25 16:59:18 +0100

    Create data_model.h

---
+++ data_model.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Arduino.h>
+
+struct TrainerData {
+    int power = 0;
+    int cadence = 0;
+    float speed = 0.0f;
+    uint32_t timestamp = 0;
+};
+
+extern TrainerData g_trainerData;
+
+void updateTrainerData(int power, int cadence, float speed);
