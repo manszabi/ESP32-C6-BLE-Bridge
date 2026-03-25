a6e401d17db8e5f6879e8fd415bf3010fbbb7879
manszabi <42097377+manszabi@users.noreply.github.com>
2026-03-25 16:59:00 +0100

    Create data_model.cpp

---
+++ data_model.cpp
@@ -0,0 +1,10 @@
+#include "data_model.h"
+
+TrainerData g_trainerData;
+
+void updateTrainerData(int power, int cadence, float speed) {
+    g_trainerData.power = power;
+    g_trainerData.cadence = cadence;
+    g_trainerData.speed = speed;
+    g_trainerData.timestamp = millis();
+}
