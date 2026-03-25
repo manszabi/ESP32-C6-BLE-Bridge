4b8c0b2b66ca2378832b2862874a4bb9234a18e2
manszabi <42097377+manszabi@users.noreply.github.com>
2026-03-25 20:37:38 +0100

    Update ble_peripheral.h

--- ble_peripheral.h
+++ ble_peripheral.h
@@ -1,14 +1,20 @@
 #pragma once
-
 #include <Arduino.h>
 
+void blePeripheralLoop();
+
 bool blePeripheralInit();
+void blePeripheralStartAdvertising();
+
+// Zwift / Garmin / Telefon felé küldött notify-k
+void blePeripheralSendFtms();
+void blePeripheralSendCps();
+void blePeripheralSendCsc();
+
+// Kliens-ellenőrzés
 bool isFtmsClientConnected();
 bool isCpsClientConnected();
 bool isCscClientConnected();
 
-void blePeripheralStartAdvertising();
-void blePeripheralLoop();
-
-// Zwift → ESP32 → Suito
+// Zwift → ESP32 → Suito vezérlés
 void handleControlFromClient(int targetPower);
